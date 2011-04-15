7402fb01477f866b532f45f434315c1c02c8bb39
Franz Emberger <franz.emberger@gmail.com>
2011-04-15 11:53:23 +0200

    Make use of the sync wrapper in the python api
    Thanks anders

--- src/xmms2_dj/apps/dj/xmms2.py
+++ src/xmms2_dj/apps/dj/xmms2.py
@@ -6,13 +6,15 @@ Beinhaltet Helfer-Klassen für die Steuerung des XMMS2 Servers
 
 import xmmsclient
 import xmmsclient.collections as coll
+from xmmsclient import XMMSSync, XMMSError
+
 import sys
 import os 
 class XmmsClient(object):
     """A wrapper to the xmmsclient
     """
     def __init__(self, name):
-        self.client = xmmsclient.XMMS(name)
+        self.client = XMMSSync(name)
         self.client.connect(os.getenv("XMMS_PATH"))
 
     def current(self):
@@ -20,14 +22,10 @@ class XmmsClient(object):
            
            @return songtitle or None (if nothing is played at the moment)
         """
-        result = self.client.playback_current_id()
-        result.wait()
-        id = result.value()
+        id = self.client.playback_current_id()
         if id == 0:
             return None
-        result = self.client.medialib_get_info(id)
-        result.wait()
-        info = result.value()
+        info = self.client.medialib_get_info(id)
         if info != None:
             return (id, info["artist"], info["title"])
         else:
@@ -36,36 +34,29 @@ class XmmsClient(object):
     def play(self):
         """start playback
         """
-        result = self.client.playback_start()
-        result.wait()
+        self.client.playback_start()
 
     def pause(self):
         """pause playback
         """
-        result = self.client.playback_pause()
-        result.wait()
+        self.client.playback_pause()
 
     def stop(self):
         """stop playback
         """
-        result = self.client.playback_stop()
-        result.wait()
+        self.client.playback_stop()
 
     def prev(self):
         """Jump to previous playlist entry
         """
-        result = self.client.playlist_set_next_rel(-1)
-        result.wait()
-        result = self.client.playback_tickle()
-        result.wait()
+        self.client.playlist_set_next_rel(-1)
+        self.client.playback_tickle()
 
     def next(self):
         """Jump to the next playlist entry
         """
-        result = self.client.playlist_set_next_rel(1)
-        result.wait()
-        result = self.client.playback_tickle()
-        result.wait()
+        self.client.playlist_set_next_rel(1)
+        self.client.playback_tickle()
 
     
     def jump(self, id):
@@ -73,10 +64,8 @@ class XmmsClient(object):
 
            @param id number of the playlist entry to jump to
         """
-        result = self.client.playlist_set_next(id)
-        result.wait()
-        result = self.client.playback_tickle()
-        result.wait()
+        self.client.playlist_set_next(id)
+        self.client.playback_tickle()
 
     def list(self, with_mlibid=False):
         """list playlist entries
@@ -84,15 +73,11 @@ class XmmsClient(object):
            @return an array of dicts for example:
                [{'id': 1, 'artist': 'Machine Head', 'title': Imperium', 'duration': 450000},]
         """
-        result = self.client.playlist_list_entries()
-        result.wait()
-        id_list = result.value()
+        id_list = self.client.playlist_list_entries()
         title_list = []
         pl_id = 0
         for id in id_list:
-            result = self.client.medialib_get_info(id)
-            result.wait()
-            info = result.value()
+            info = self.client.medialib_get_info(id)
             entry = {'id': pl_id,
                             'artist': info.get("artist"),
                             'title': info.get("title"),
@@ -111,40 +96,29 @@ class XmmsClient(object):
            @param keys list of keys which the result should include
            @return list of dicts
         """
-        result = self.client.coll_query_infos(c, keys)
-        result.wait()
-        return result.value()
+        return self.client.coll_query_infos(c, keys)
 
     def playlist_add_id(self, id):
         """Add a medialib ID to the playlist"""
-        result = self.client.playlist_add_id(id)
-        result.wait()
-        return result.value()
+        return self.client.playlist_add_id(id)
 
     def playlist_add_collection(self, collection):
         """Add all entries in a collection to the playlist"""
-        result = self.client.playlist_add_collection(collection)
-        result.wait()
-        return result.value()
+        return self.client.playlist_add_collection(collection)
 
     def playlist_remove_id(self, id):
         """Remove an entry from the active playlist"""
-        result = self.client.playlist_remove_entry(id)
-        result.wait()
-        return result.value()
+        return self.client.playlist_remove_entry(id)
 
     def playlist_clear(self):
         """Remove all entries from the active playlist"""
-        result = self.client.playlist_clear()
-        result.wait()
-        return result.value()
+        return self.client.playlist_clear()
 
     def playlist_list(self):
         """List all playlists"""
         result = self.client.playlist_list()
-        result.wait()
         list = []
-        for entry in result.value():
+        for entry in result:
             # by the xmms client conventions, lists starting with an underscore
             # may not be visible to users.
             if not entry.startswith('_'):
@@ -154,18 +128,14 @@ class XmmsClient(object):
 
     def playlist_active(self):
         """Return the name of the active playlist"""
-        result = self.client.playlist_current_active()
-        result.wait()
-        return result.value()
+        return self.client.playlist_current_active()
 
     def playlist_load(self, playlist):
         """Load the specified playlist
            
            @param playlist name of the playlist as string
         """
-        result = self.client.playlist_load(playlist)
-        result.wait()
-        return result.value()
+        return self.client.playlist_load(playlist)
 
 
     def playlist_create(self, playlist):
@@ -173,16 +143,12 @@ class XmmsClient(object):
            
            @param playlist name of the playlist
         """
-        result = self.client.playlist_create(playlist)
-        result.wait()
-        return result.value()
+        return self.client.playlist_create(playlist)
 
     def playlist_remove(self, playlist):
         """Löscht die aktuelle Playlist
         """
-        result = self.client.playlist_remove(playlist)
-        result.wait()
-        return result.value();
+        return self.client.playlist_remove(playlist)
 
     def playlist_move(self, cur_pos, new_pos):
         """Move a playlist entry to a new position
@@ -194,25 +160,19 @@ class XmmsClient(object):
         if new_pos > max_pos or new_pos < 0 or cur_pos < 0 or cur_pos > max_pos:
             return False
 
-        result = self.client.playlist_move(cur_pos, new_pos)
-        result.wait()
-        return result.value()
+        return self.client.playlist_move(cur_pos, new_pos)
 
     def playlist_shuffle(self):
         """Shuffle the active playlist
         """
-        result = self.client.playlist_shuffle()
-        result.wait()
-        return result.value()
+        return self.client.playlist_shuffle()
 
     def get_info(self, id):
         """Return information on a title
 
            @param id title id
         """
-        result = self.client.medialib_get_info(id)
-        result.wait()
-        return result.value()
+        return self.client.medialib_get_info(id)
 
     def volume_up(self, vol_add, channel=None):
         """Rise volume
@@ -222,23 +182,19 @@ class XmmsClient(object):
 
            @return <code>True</code> on success, otherwise <code>False</code>
         """
-        result = self.client.playback_volume_get()
-        result.wait()
-        volume = result.value()
+        volume = self.volume_get()
         # curr_vol wird als dict zurück gegeben, das die Lautstärke für die
         # Kanäle 'right' und 'left' enthält.
         # Falls keine Lautstärke ermittelt werden konnte, enthält es einen
         # String mit einer Fehlermeldung.
-        if not isinstance(volume, dict):
+        if volume is None:
             return False
 
         if channel is None:
             for ch, value in volume.items():
-                result = self.client.playback_volume_set(ch, value + vol_add)
-                result.wait()
+                self.client.playback_volume_set(ch, value + vol_add)
         else:
-            result = self.client.playback_volume_set(channel, volume[channel]+vol_add)
-            result.wait()
+            self.client.playback_volume_set(channel, volume[channel]+vol_add)
 
         return True
 
@@ -250,20 +206,16 @@ class XmmsClient(object):
 
            @return <code>True</code> on success, otherwise <code>False</code>
         """
-        result = self.client.playback_volume_get()
-        result.wait()
-        volume = result.value()
+        volume = self.volume_get()
         # siehe volume_up(..)
-        if not isinstance(volume, dict):
+        if volume is None:
             return False
 
         if channel is None:
             for ch, value in volume.items():
-                result = self.client.playback_volume_set(ch, value - vol_dec)
-                result.wait()
+                self.client.playback_volume_set(ch, value - vol_dec)
         else:
-            result = self.client.playback_volume_set(channel, volume[channel])
-            result.wait()
+            self.client.playback_volume_set(channel, volume[channel])
 
         return True
 
@@ -273,6 +225,9 @@ class XmmsClient(object):
            @return the current volume, for example
                    {'left': 100, 'right': 100}
         """
-        result = self.client.playback_volume_get()
-        result.wait()
-        return result.value()
+        try:
+            volume = self.client.playback_volume_get()
+        except XMMSError:
+            volume = None
+
+        return volume
